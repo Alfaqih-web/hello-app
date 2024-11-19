27ad6fb92c626b8a01f6f090071e81031e5307e6
Alfaqih-web <alfaqihbackup@gmail.com>
2024-11-19 14:38:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,13 @@ st.info(" This app is to predict what type of disease you have through explain t
 
 with st.expander("Data"):
   st.write("**Raw Data**")
-
-df = pd.read_csv('https://raw.githubusercontent.com/Alfaqih-web/hello-app/refs/heads/main/Symptom2Disease.csv')
-df
-
-st.write("**X**")
-X = df.drop("label", axis=1)
-X
-
-st.write("**Y**")
-y = df.label
-y
+  df = pd.read_csv('https://raw.githubusercontent.com/Alfaqih-web/hello-app/refs/heads/main/Symptom2Disease.csv')
+  df
+  
+  st.write("**X**")
+  X = df.drop("label", axis=1)
+  X
+  
+  st.write("**Y**")
+  y = df.label
+  y
