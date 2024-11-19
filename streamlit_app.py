ff2a632a2ebfbf3d857648be23b0c140c12316c0
Alfaqih-web <alfaqihbackup@gmail.com>
2024-11-19 14:27:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import os
 
 st.write(" 🦅🦅🦅 Hello World!!!!!")
+
+df = pd.read.csv("https://raw.githubusercontent.com/Alfaqih-web/hello-app/refs/heads/main/Symptom2Disease.csv")
+df
