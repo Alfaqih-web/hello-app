3f9c69fe1876cf0ff99836ee46b4ec2e3e74fed3
Alfaqih-web <alfaqihbackup@gmail.com>
2024-11-19 14:33:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import os
 
 st.write(" 🦅🦅🦅 Hello Wthis is the App testing for the Diosease prediction")
 
-st.info(" This app is to predict what type of disease you have through explain the symptoms experienced"
+st.info(" This app is to predict what type of disease you have through explain the symptoms experienced")
 
 df = pd.read_csv('https://raw.githubusercontent.com/Alfaqih-web/hello-app/refs/heads/main/Symptom2Disease.csv')
 df
